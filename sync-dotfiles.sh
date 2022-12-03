d3760850520df2d015d82722bc479e1b0eea9a01
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2022-12-02 19:59:51 -0500

    Make `sync-dotfiles.sh` handle git submodules.

--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -39,7 +39,9 @@ sync_repo() {
     return 1
   fi
   # Otherwise attempt to update.
-  git_output=$(git $gitcolor pull --recurse-submodules=on-demand 2>&1)
+  git_output=$(git $gitcolor pull --recurse-submodules=on-demand 2>&1 &&
+               git $gitcolor submodule sync --recursive 2>&1 &&
+               git $gitcolor submodule update --init --recursive 2>&1)
   return_code=$?
   if [ $return_code -ne 0 ]; then
     printf "[${txtred}FAIL${txtrst}]${txtred}\n"
