0cce95c95a3f9f2daef4a45597c2880ad8f8c482
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2024-12-23 18:39:37 -0500

    sync-dotfiles.sh: replace 'echo -e' with 'printf' ('echo -e' is a bash extension)

--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -8,10 +8,10 @@ self=$(basename "$0")
 
 # Colorize output if stdout/stderr is a terminal.
 [ -t 1 ] && [ -t 2 ] && stdout_is_tty=true || stdout_is_tty=false
-$stdout_is_tty && txtred="$(echo  -e '\033[01;31m')"  || txtred=
-$stdout_is_tty && txtgrn="$(echo  -e '\033[01;32m')"  || txtgrn=
-$stdout_is_tty && txtemph="$(echo -e '\033[01;35m')"  || txtemph=
-$stdout_is_tty && txtrst="$(echo  -e '\033[00m')"     || txtrst=
+$stdout_is_tty && txtred="$(printf  '\033[01;31m')"  || txtred=
+$stdout_is_tty && txtgrn="$(printf  '\033[01;32m')"  || txtgrn=
+$stdout_is_tty && txtemph="$(printf '\033[01;35m')"  || txtemph=
+$stdout_is_tty && txtrst="$(printf  '\033[00m')"     || txtrst=
 $stdout_is_tty && gitcolor='-c color.ui=always' || gitcolor=
 die() { printf "\n$txtred%s: %s$txtrst\n" "$self" "$*"; exit 1; }
 
